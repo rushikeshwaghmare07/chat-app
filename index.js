93cefdf4fb7eac100865ff9c417aec9a2fcf8e2c
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-06-29 15:13:23 +0530

    added a two side conversation logic

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const { Socket } = require("dgram");
 const express = require("express");
 const http = require("http");
 require("dotenv").config();
 const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000
+const io = new Server(server);
+
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  })
+});
 
 app.use(express.static("./public"));
 
