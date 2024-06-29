ef285ef0e6cdb1db36c45fbc245ec4132dbdd3e7
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-06-29 14:09:04 +0530

    added html file for server side rendering

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const http = require("http");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+})
 
 server.listen(PORT, () => {
   console.log(`Server is listening on PORT: ${PORT}`);
